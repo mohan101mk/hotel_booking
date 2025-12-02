512fb3aa213dfb95e00c7ff5700c50d1e74d4ae0
Mohan Kumar C R <mohanmk@Mohans-MacBook-Pro.local>
2025-12-02 16:04:09 +0530

    cors error fixed

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectCloudinary();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://hotel-booking-teal-phi.vercel.app', credentials: true }));
 
 app.use(express.json());
 
